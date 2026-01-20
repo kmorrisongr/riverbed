1de730f5ed29243a0cb969d4b699985a7889061a
kmorrisongr <mckinleybagel@pm.me>
2026-01-19 20:31:39 -0500

    remove unused re-exports

--- server/src/network/mod.rs
+++ server/src/network/mod.rs
@@ -2,6 +2,3 @@ pub mod block_interactions;
 pub mod chunk_streaming;
 pub mod lightyear_server;
 pub mod renet_chunk_server;
-
-pub use chunk_streaming::{ChunkDeliveryTracker, ChunkStreamingPlayer, ChunkStreamingPlugin};
-pub use renet_chunk_server::{ChunkServerConfig, RenetChunkServerPlugin};
